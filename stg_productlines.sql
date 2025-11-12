9b7facc3b365fef517121250d184e75edaebbc83
Mukund S Ganig <mukunds.btech23@rvu.edu.in>
2025-11-12 19:36:31 +0530

    Create stg_productlines.sql for product lines view

---
+++ stg_productlines.sql
@@ -0,0 +1,7 @@
+{{ config(materialized='view') }}
+
+select
+    PRODUCTLINE as product_line,
+    TEXTDESCRIPTION as text_description,
+    HTMLDESCRIPTION as html_description
+from {{ source('sales', 'productlines') }}
